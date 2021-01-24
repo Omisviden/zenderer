da83a4a5066a21110f1b017b62b614752b559335
Matthew J. Berger <matthewberger@nevada.unr.edu>
2021-01-24 00:00:56 -0700

    Move error message to separate field

--- crates/obsidian_app/src/logger.rs
+++ crates/obsidian_app/src/logger.rs
@@ -4,7 +4,8 @@ use std::{fs::File, path::Path};
 
 pub fn create_logger(path: impl AsRef<Path>) -> Result<()> {
     let name = path.as_ref().display().to_string();
-    let file = File::create(path).context(format!("Failed to create log file named: {}", name))?;
+    let error_message = format!("Failed to create log file named: {}", name);
+    let file = File::create(path).context(error_message)?;
     CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed),
         WriteLogger::new(LevelFilter::max(), Config::default(), file),
