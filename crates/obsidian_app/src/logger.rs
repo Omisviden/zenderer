840b8aa36b04e9bf76a2a5f183d877286e2ec1bd
Matthew J. Berger <matthewberger@nevada.unr.edu>
2021-01-23 23:49:49 -0700

    Simplify logger creation

--- crates/obsidian_app/src/logger.rs
+++ crates/obsidian_app/src/logger.rs
@@ -5,16 +5,13 @@ use std::fs::File;
 pub const LOG_FILE: &str = "obsidian.log";
 
 pub fn create_logger() -> Result<()> {
+    let file = File::create(LOG_FILE).context(format!(
+        "Failed to create log file named: {}",
+        LOG_FILE.to_string()
+    ))?;
     CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed),
-        WriteLogger::new(
-            LevelFilter::max(),
-            Config::default(),
-            File::create(LOG_FILE).context(format!(
-                "Failed to create log file named: {}",
-                LOG_FILE.to_string()
-            ))?,
-        ),
+        WriteLogger::new(LevelFilter::max(), Config::default(), file),
     ])?;
     Ok(())
 }
